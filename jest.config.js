f4e5d96dc207f0e0cab93aef0119aca05363bd6d
matt-clarson <clarson469@googlemail.com>
2020-04-13 22:08:49 +0100

    added coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,17 @@
-module.exports = {
+const config = {
   roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
   testRegex: '(/__tests__/.*|(\\.|/)test)\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
+
+if (process.env.USE_COVERAGE) {
+  Object.assign(config, {
+    collectCoverage: true,
+    coveragePathIgnorePatterns: ['/node_modules/', 'index.ts']
+  });
+}
+
+module.exports = config;
